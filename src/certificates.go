d39ccf2275268a0313da2812af909971e0927f55
Casey Witt <kcwitt@gmail.com>
2017-05-19 18:12:59 +0800

    Compilable version

--- src/certificates.go
+++ src/certificates.go
@@ -1,307 +1,210 @@
 package kubesec
 
 import (
+	"bytes"
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
 	"crypto/x509"
+	"crypto/x509/pkix"
 	"encoding/pem"
-	"flag"
-	"io"
 	"io/ioutil"
+	"math/big"
 	"net"
 	"net/mail"
 	"os"
 	"path/filepath"
 	"strings"
-	"time"
 )
 
-func createCA(args []string, path string, defaultDn string, defaultValidity int) {
-	fs := flag.NewFlagSet("ca", flag.PanicOnError)
-	dn := fs.String("dn", defaultDn, "certificate subject")
-	maxPathLength := fs.Int("maxPathLength", 0, "max path length")
-	validity := fs.Int("validity", defaultValidity, "ca validity in days")
-	overwrite := fs.Bool("overwrite", false, "overwrite any existing files")
+var serialNumberLimit = new(big.Int).Lsh(big.NewInt(1), 128)
 
-	err := fs.Parse(args)
+type certManifest struct {
+	path string // path of new certificate directory relative to root
+
+	cert        *x509.Certificate
+	key         *ecdsa.PrivateKey
+	signingCert *x509.Certificate
+	signingKey  *ecdsa.PrivateKey
+}
+
+func generateSerial() *big.Int {
+	serial, err := rand.Int(rand.Reader, serialNumberLimit)
 	if err != nil {
-		errorLog.Fatalf("Failed to parse command line arguments: %s", err)
+		errorLog.Fatalf("Failed to generate serial number: %s", err)
 	}
+	return serial
+}
 
-	if len(fs.Args()) > 1 {
-		errorLog.Fatalf("Invalid path %s", strings.Join(fs.Args(), ","))
-	} else if len(fs.Args()) == 1 {
-		path = fs.Arg(0)
+func generateKey() *ecdsa.PrivateKey {
+	key, err := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
+	if err != nil {
+		errorLog.Fatalf("Failed to generate private key: %s", err)
 	}
+	return key
+}
 
-	infoLog.Printf("Creating Certificate Authority %s with Subject: %s\n", path, *dn)
-
-	if !*overwrite {
-		checkExisting(filepath.Join(root, path))
-	}
-
-	ca := filepath.Dir(path)
-	var caCert *x509.Certificate
-	var caKey *ecdsa.PrivateKey
-	if ca != "." {
-		caCert = parseCert(ca)
-		if !caCert.IsCA {
-			errorLog.Fatalf("Certificate %s is not a certificate authority", ca)
-		} else if !(caCert.MaxPathLen > 0) {
-			errorLog.Fatalf("Certificate Authority %s can't sign other certificate authorities (maxPathLength exceeded)", ca)
+func parseDN(issuer pkix.Name, dn string) pkix.Name {
+	infoLog.Printf("Parsing Distinguished Name: %s\n", dn)
+	issuer.CommonName = ""
+	for _, element := range strings.Split(strings.Trim(dn, "/"), "/") {
+		value := strings.Split(element, "=")
+		if len(value) != 2 {
+			errorLog.Fatalf("Failed to parse distinguised name: malformed element %s in dn", element)
 		}
-		*maxPathLength = caCert.MaxPathLen - 1
-		caKey = parseKey(ca)
-	}
-
-	key, derKey, err := generatePrivateKey()
-	if err != nil {
-		errorLog.Fatalf("Error generating private key: %s", err)
-	}
-
-	notBefore := time.Now().UTC()
-	notAfter := notBefore.AddDate(0, 0, *validity)
-	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
-	if err != nil {
-		errorLog.Fatalf("Failed to generate serial number: %s", err)
-	}
-
-	template := x509.Certificate{
-		SerialNumber:          serialNumber,
-		Subject:               *parseDn(caCert, *dn),
-		NotBefore:             notBefore,
-		NotAfter:              notAfter,
-		BasicConstraintsValid: true,
-		IsCA:           true,
-		MaxPathLen:     *maxPathLength,
-		MaxPathLenZero: *maxPathLength == 0,
-		KeyUsage:       x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
-	}
-
-	if caCert == nil {
-		caCert = &template
-		caKey = key
-	}
-
-	derCert, err := x509.CreateCertificate(rand.Reader, &template, caCert, &key.PublicKey, caKey)
-	if err != nil {
-		errorLog.Fatalf("Failed to create CA Certificate: %s", err)
-	}
-	saveCert(path, derCert)
-	saveKey(path, derKey)
-	if caCert != &template {
-		copyFile(filepath.Join(filepath.Dir(path), "ca.pem"), filepath.Join(path, "ca.pem"), publicPerms)
-	} else {
-		copyFile(filepath.Join(path, path+".crt"), filepath.Join(path, "ca.pem"), publicPerms)
-	}
-	infoLog.Printf("Finished Creating Certificate Authority %s with Subject: %s\n", path, *dn)
-}
-
-func createCertificate(args []string, path string, defaultDn string, defaultValidity int, keyUsage x509.KeyUsage, extKeyUsage []x509.ExtKeyUsage) {
-	fs := flag.NewFlagSet("server", flag.PanicOnError)
-	dn := fs.String("dn", defaultDn, "certificate subject")
-	san := fs.String("san", "", "subject alternative names")
-	validity := fs.Int("validity", defaultValidity, "certificate validity in days")
-	overwrite := fs.Bool("overwrite", false, "overwrite any existing files")
-	nolocalhost := fs.Bool("no-localhost", false, "do not include localhost Subject Alternative Names by default")
-
-	err := fs.Parse(args)
-	if err != nil {
-		errorLog.Fatalf("Failed to parse command line argumanets: %s", err)
-	}
-
-	if len(fs.Args()) > 1 {
-		errorLog.Fatalf("Invalid path %s", strings.Join(fs.Args(), ","))
-	} else if len(fs.Args()) == 1 {
-		path = fs.Arg(0)
-	}
-
-	infoLog.Printf("Creating Certificate %s with Subject: %s\n", path, *dn)
-
-	if !*overwrite {
-		checkExisting(path)
-	}
-
-	ca := filepath.Dir(path)
-
-	caCert := parseCert(ca)
-	if !caCert.IsCA {
-		errorLog.Fatalf("Certificate %s is not a certificate authority", filepath.Dir(path))
-	}
-	caKey := parseKey(ca)
-
-	key, derKey, err := generatePrivateKey()
-	if err != nil {
-		errorLog.Fatalf("Error generating private key: %s", err)
-	}
-
-	notBefore := time.Now().UTC().Add(-10 * time.Minute) // -10 min to mitigate clock skew
-	notAfter := notBefore.AddDate(0, 0, *validity).Add(10 * time.Minute)
-	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
-	if err != nil {
-		errorLog.Fatalf("Failed to generate serial number: %s", err)
-	}
-
-	template := x509.Certificate{
-		SerialNumber:   serialNumber,
-		Subject:        *parseDn(caCert, *dn),
-		NotBefore:      notBefore,
-		NotAfter:       notAfter,
-		IsCA:           false,
-		KeyUsage:       keyUsage,
-		ExtKeyUsage:    extKeyUsage,
-		EmailAddresses: []string{},
-		IPAddresses:    []net.IP{net.ParseIP("127.0.0.1"), net.ParseIP("::1")},
-		DNSNames:       []string{"localhost"},
-	}
-
-	if *nolocalhost {
-		template.IPAddresses = []net.IP{}
-		template.DNSNames = []string{}
-	}
-	parseSubjectAlternativeNames(*san, &template)
-
-	derCert, err := x509.CreateCertificate(rand.Reader, &template, caCert, &key.PublicKey, caKey)
-	if err != nil {
-		errorLog.Fatalf("Failed to create Server Certificate %s: %s", path, err)
-	}
-
-	saveCert(path, derCert)
-	saveKey(path, derKey)
-	copyFile(filepath.Join(filepath.Dir(path), "ca.pem"), filepath.Join(path, "ca.pem"), publicPerms)
-	infoLog.Printf("Finished Creating Certificate %s with Subject: %s\n", path, *dn)
-}
-
-func parseSubjectAlternativeNames(san string, template *x509.Certificate) {
-	infoLog.Printf("Parsing Subject Alternative Names: %s\n", san)
-	if san != "" {
-		for _, h := range strings.Split(san, ",") {
-			infoLog.Printf("Parsing %s\n", h)
-			if ip := net.ParseIP(h); ip != nil {
-				template.IPAddresses = append(template.IPAddresses, ip)
-			} else if email := parseEmailAddress(h); email != nil {
-				template.EmailAddresses = append(template.EmailAddresses, email.Address)
+		switch strings.ToUpper(value[0]) {
+		case "CN": // commonName
+			issuer.CommonName = value[1]
+		case "C": // countryName
+			if value[1] != "" {
+				issuer.Country = value[1:1]
 			} else {
-				template.DNSNames = append(template.DNSNames, h)
+				issuer.Country = nil
 			}
+		case "L": // localityName
+			if value[1] != "" {
+				issuer.Locality = value[1:1]
+			} else {
+				issuer.Locality = nil
+			}
+		case "ST": // stateOrProvinceName
+			if value[1] != "" {
+				issuer.Province = value[1:1]
+			} else {
+				issuer.Province = nil
+			}
+		case "O": // organizationName
+			if value[1] != "" {
+				issuer.Organization = value[1:1]
+			} else {
+				issuer.Organization = nil
+			}
+		case "OU": // organizationalUnitName
+			if value[1] != "" {
+				issuer.OrganizationalUnit = value[1:1]
+			} else {
+				issuer.OrganizationalUnit = nil
+			}
+		default:
+			errorLog.Fatalf("Failed to parse distinguised name: unknown element %s", element)
+		}
+	}
+	if issuer.CommonName == "" {
+		errorLog.Fatalf("Failed to parse common name from %s", dn)
+	}
+	return issuer
+}
+
+func (manifest *certManifest) parseSAN(sans string) {
+	infoLog.Printf("Parsing Subject Alternative Names: %s\n", sans)
+	for _, h := range strings.Split(sans, ",") {
+		if ip := net.ParseIP(h); ip != nil {
+			manifest.cert.IPAddresses = append(manifest.cert.IPAddresses, ip)
+		} else if email := parseEmailAddress(h); email != nil {
+			manifest.cert.EmailAddresses = append(manifest.cert.EmailAddresses, email.Address)
+		} else {
+			manifest.cert.DNSNames = append(manifest.cert.DNSNames, h)
 		}
 	}
 }
 
-// implemented as a seperate function because net.mail.ParseAddress
-// panics on malformed addresses
 func parseEmailAddress(address string) (email *mail.Address) {
+	// implemented as a seperate function because net.mail.ParseAddress
+	// panics on malformed addresses (appears to be a bug)
 	defer func() {
 		if recover() != nil {
 			email = nil
 		}
 	}()
+	email, err := mail.ParseAddress(address)
+	if err != nil || email == nil {
+		email = nil
+	}
+	return
+}
+
+func (manifest *certManifest) sign() {
+	//manifest.cert.SignatureAlgorithm = x509.ECDSAWithSHA384
+	derCert, err := x509.CreateCertificate(rand.Reader, manifest.cert, manifest.signingCert, manifest.key.PublicKey, manifest.signingKey)
+	if err != nil {
+		errorLog.Fatalf("Failed to sign certificate: %s", err)
+	}
+	cert, err := x509.ParseCertificate(derCert)
+	if err != nil {
+		errorLog.Fatalf("Failed to load signed certificate: %s", err)
+	}
+	manifest.cert = cert
+}
+
+func (manifest *certManifest) save() {
+	baseName := filepath.Base(manifest.path)
+	manifest.saveCert(filepath.Join(manifest.path, baseName+".pem"))
+	manifest.saveKey(filepath.Join(manifest.path, baseName+"-key.pem"))
+	manifest.saveCA(filepath.Join(manifest.path, "ca.pem"))
+}
+
+func (manifest *certManifest) saveCert(path string) {
+	infoLog.Printf("Saving %s\n", filepath.Join(root, path))
+	file, err := os.OpenFile(path, fileCreateFlags, publicPerms)
+	if err != nil {
+		errorLog.Fatalf("Failed to open file %s for writing: %s", filepath.Join(root, path), err)
+	}
+	defer func() {
+		if err := file.Close(); err != nil {
+			errorLog.Fatalf("Failed to close file %s after writing: %s", filepath.Join(root, path), err)
+		}
+	}()
+	if err := pem.Encode(file, &pem.Block{Type: "CERTIFICATE", Bytes: manifest.cert.Raw}); err != nil {
+		errorLog.Fatalf("Failed to pem encode %s: %s", filepath.Join(root, path), err)
+	}
+	if _, err := file.Write(concatCAs(filepath.Dir(manifest.path)).Bytes()); err != nil {
+		errorLog.Fatalf("Failed to concat CAs %s: %s", filepath.Join(root, path), err)
+	}
+}
+
+func (manifest *certManifest) saveKey(path string) {
+	infoLog.Printf("Saving %s\n", filepath.Join(root, path))
+	file, err := os.OpenFile(path, fileCreateFlags, privatePerms)
+	if err != nil {
+		errorLog.Fatalf("Failed to open file %s for writing: %s", filepath.Join(root, path), err)
+	}
+	defer func() {
+		if err = file.Close(); err != nil {
+			errorLog.Fatalf("Failed to close file %s after writing: %s", path, err)
+		}
+	}()
+	der, err := x509.MarshalECPrivateKey(manifest.key)
+	if err != nil {
+		errorLog.Fatalf("Failed to der encode private key %s: %s", path, err)
+	}
+	if err = pem.Encode(file, &pem.Block{Type: "EC PRIVATE KEY", Bytes: der}); err != nil {
+		errorLog.Fatalf("Failed to pem encode private key %s: %s", path, err)
+	}
+}
+
+func (manifest *certManifest) saveCA(path string) {
+	var data []byte
 	var err error
-	email, err = mail.ParseAddress(address)
-	if err == nil && email != nil {
-		return email
-	}
-	return nil
-}
-
-func generatePrivateKey() (*ecdsa.PrivateKey, []byte, error) {
-	key, err := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
-	if err != nil {
-		return nil, nil, err
-	}
-	derKey, err := x509.MarshalECPrivateKey(key)
-	if err != nil {
-		return nil, nil, err
-	}
-	return key, derKey, nil
-}
-
-func checkExisting(path string) {
-	fullPath := filepath.Join(path, filepath.Base(path))
-	const errMsg = "Skipping creation of %s because file %s already exists.\nUse the \"-overwrite\" option to overwrite the existing file."
-	if _, err := os.Stat(fullPath + ".pem"); err == nil {
-		errorLog.Fatalf(errMsg, path, fullPath+".pem")
-	}
-	if _, err := os.Stat(fullPath + "-key.pem"); err == nil {
-		errorLog.Fatalf(errMsg, path, fullPath+"-key.pem")
-	}
-	if _, err := os.Stat(filepath.Join(path, "ca.pem")); err == nil {
-		errorLog.Fatalf("Skipping creation of %s because file %s already exists.\nUse the \"-overwrite\" option to overwrite the existing file.", path, filepath.Join(path, "ca.pem"))
-	}
-}
-
-func createDirectory(directory string) {
-	if _, err := os.Stat(directory); os.IsNotExist(err) {
-		var publicPerms os.FileMode = 0755
-		if err := os.MkdirAll(directory, publicPerms); err != nil {
-			errorLog.Fatalf("Error creating directory ./%s: %s", directory, err.Error())
-		}
-	}
-}
-
-func saveCert(directory string, derCert []byte) {
-	createDirectory(directory)
-
-	fileName := filepath.Join(directory, filepath.Base(directory)+".pem")
-
-	infoLog.Printf("Saving %s\n", fileName)
-
-	certFile, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE, publicPerms)
-	if err != nil {
-		errorLog.Fatalf("Failed to open %s for writing: %s", fileName, err)
-	}
-	defer func() {
-		if err := certFile.Close(); err != nil {
-			errorLog.Fatalf("Failed to save %s: %s", fileName, err)
-		}
-	}()
-	if err := pem.Encode(certFile, &pem.Block{Type: "CERTIFICATE", Bytes: derCert}); err != nil {
-		errorLog.Fatalf("Failed to marshall %s: %s", fileName, err)
-	}
-	if filepath.Dir(directory) != "." {
-		caFile, err := os.Open(filepath.Join(filepath.Dir(directory), filepath.Base(filepath.Dir(directory))) + ".pem")
+	switch {
+	case manifest.path == "ca": // do nothing; it is a self signed cert named "ca", so ca.pem already exists
+		return
+	case filepath.Dir(manifest.path) == ".": // this is a root ca, so just copy the certificate to ca.pem
+		data, err = ioutil.ReadFile(filepath.Join(root, manifest.path, filepath.Base(manifest.path)+".pem"))
 		if err != nil {
-			errorLog.Fatalf("Failed to open ca certificate: %s", err)
+			errorLog.Fatalf("Error reading %s: %s", filepath.Join(root, manifest.path, filepath.Base(manifest.path)+".pem"), err)
 		}
-		defer func() {
-			if err = caFile.Close(); err != nil {
-				errorLog.Fatalf("Failed to close %s: %s", filepath.Join(filepath.Dir(directory), filepath.Base(filepath.Dir(directory)))+".pem", err)
-			}
-		}()
-		_, err = io.Copy(certFile, caFile)
-		if err != nil {
-			errorLog.Fatalf("Failed to concat ca certificates: %s", err)
-		}
-		err = certFile.Sync()
-		if err != nil {
-			errorLog.Fatalf("Failed to sync certificate file: %s", err)
-		}
+	case filepath.Base(manifest.path) == "ca": // required to avoid case where certificate and it's ca have the same file name (unless it is self signed)
+		errorLog.Fatalf("Error creating %s: certs that are not self-signed can't be named \"ca\".", path)
+	default: // concat all certs in manifest.path
+		data = concatCAs(manifest.path).Bytes()
+	}
+	if err = ioutil.WriteFile(path, data, publicPerms); err != nil {
+		errorLog.Fatalf("Error writing %s: %s", path, err)
 	}
 }
 
-func saveKey(directory string, derKey []byte) {
-
-	fileName := filepath.Join(directory, filepath.Base(directory)+"-key.pem")
-
-	infoLog.Printf("Saving %s\n", fileName)
-
-	keyFile, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, privatePerms)
-	if err != nil {
-		errorLog.Fatalf("Failed to open %s for writing: %s", fileName, err)
-	}
-	defer func() {
-		if err := keyFile.Close(); err != nil {
-			errorLog.Fatalf("Failed to close %s: %s", fileName, err)
-		}
-	}()
-	if err := pem.Encode(keyFile, &pem.Block{Type: "EC PRIVATE KEY", Bytes: derKey}); err != nil {
-		errorLog.Fatalf("Failed to marshall %s: %s", fileName, err)
-	}
-}
-
-func parseCert(path string) *x509.Certificate {
-	der, err := ioutil.ReadFile(filepath.Join(path, filepath.Base(path)+".crt"))
+func readCert(path string) *x509.Certificate {
+	der, err := ioutil.ReadFile(path)
 	if err != nil {
 		errorLog.Fatalf("Failed to read certificate file %s: %s", filepath.Join(path, filepath.Base(path)+".crt"), err)
 	}
@@ -316,8 +219,8 @@ func parseCert(path string) *x509.Certificate {
 	return crt
 }
 
-func parseKey(path string) *ecdsa.PrivateKey {
-	der, err := ioutil.ReadFile(filepath.Join(path, filepath.Base(path)+".key"))
+func readKey(path string) *ecdsa.PrivateKey {
+	der, err := ioutil.ReadFile(path + "-key.pem")
 	if err != nil {
 		errorLog.Fatalf("Failed to read private key file %s: %s", filepath.Join(path, filepath.Base(path)+".key"), err)
 	}
@@ -330,4 +233,22 @@ func parseKey(path string) *ecdsa.PrivateKey {
 		errorLog.Fatalf("Failed to parse private key for %s: %s", filepath.Join(path, filepath.Base(path)+".key"), err)
 	}
 	return key
+}
+
+func concatCAs(path string) *bytes.Buffer {
+	var buf *bytes.Buffer
+	for file := filepath.Dir(path); file != "."; file = filepath.Dir(file) {
+		der, err := ioutil.ReadFile(path)
+		if err != nil {
+			errorLog.Fatalf("Failed to read certificate file %s: %s", filepath.Join(root, path, filepath.Base(path)+".crt"), err)
+		}
+		block, _ := pem.Decode(der)
+		if block == nil || block.Type != "CERTIFICATE" {
+			errorLog.Fatalf("Failed to decode certificate %s: %s", filepath.Join(root, path, filepath.Base(path)+".crt"), err)
+		}
+		if pem.Encode(buf, block) != nil {
+			errorLog.Fatalf("Failed to encode certificate %s: %s", filepath.Join(root, path, filepath.Base(path)+".crt"), err)
+		}
+	}
+	return buf
 }
--- src/kubesec.go
+++ src/kubesec.go
@@ -2,60 +2,68 @@ package kubesec
 
 import (
 	"crypto/x509"
+	"crypto/x509/pkix"
 	"flag"
 	"log"
-	"math/big"
 	"os"
+	"path/filepath"
+	"strings"
+	"time"
 )
 
 var (
-	infoLog  = log.New(os.Stderr, "", 0)
-	errorLog = log.New(os.Stderr, "ERROR: ", log.Lshortfile)
-
-	serialNumberLimit             = new(big.Int).Lsh(big.NewInt(1), 128)
-	privatePerms      os.FileMode = 0600
-	publicPerms       os.FileMode = 0644
-	root              string
-	overwrite         boolean
+	infoLog                     = log.New(os.Stderr, "", 0)
+	errorLog                    = log.New(os.Stderr, "ERROR: ", log.Lshortfile)
+	privatePerms    os.FileMode = 0600
+	publicPerms     os.FileMode = 0644
+	fileCreateFlags             = os.O_WRONLY | os.O_CREATE
+	root            string
+	overwrite       bool
 )
 
 func kubesec() {
-	flag.StringVar(&root, "root", "./", "path to root directory (default=./)")
-	create := flag.Bool("create", false, "create root directory if it doesn't exist (default=false)")
+	flag.StringVar(&root, "root", "./", "path to root directory (default=\"./\")")
+	flag.BoolVar(&overwrite, "overwrite", false, "overwrite existing directories (default=false)")
 	flag.Parse()
 
-	if *create {
-		err := os.MkdirAll(root, publicPerms)
-		if err != nil {
-			errorLog.Fatalf("Failed to create rood dir %s: %s", root, err)
-		}
+	if absRoot, err := filepath.Abs(root); err != nil {
+		errorLog.Fatalf("Failed to parse root directory %s: %s", root, err)
+	} else {
+		root = absRoot
+	}
+	infoLog.Printf("Using root directory: %s", root)
+	if err := os.MkdirAll(root, publicPerms); err != nil {
+		errorLog.Fatalf("Failed to create root directory %s: %s", root, err)
+	}
+	if err := os.Chdir(root); err != nil {
+		errorLog.Fatalf("Failed to set root directory to %s: %s", root, err)
 	}
 
-	var command string
+	var subCommand string
 	if len(flag.Args()) < 1 {
-		command = "help"
+		subCommand = "help"
 	} else {
-		command = flag.Args()[1]
+		subCommand = flag.Args()[1]
 	}
-	switch command {
+	switch subCommand {
 	case "ca":
-		createCA(flag.Args()[2:], "ca", "/CN=certstore-ca", 10*365+5)
+		createCA(flag.Args()[2:], "ca", "/CN=certstore-ca", time.Hour*24*(10*365+5))
 	case "ica":
-		createCA(flag.Args()[2:], "ca/ica", "/CN=certstore-ica", 5*365+5)
+		createCA(flag.Args()[2:], "ca/ica", "/CN=certstore-ica", time.Hour*24*(5*365+5))
 	case "server":
-		createCertificate(flag.Args()[2:], "ca/server", "/CN=server", 365+5,
+		createCertificate(flag.Args()[2:], "ca/server", "/CN=server", time.Hour*24*(365+5),
 			x509.KeyUsageDigitalSignature|x509.KeyUsageKeyEncipherment,
 			[]x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth})
 	case "client":
-		createCertificate(flag.Args()[2:], "ca/client", "/CN=client", 365+5,
+		createCertificate(flag.Args()[2:], "ca/client", "/CN=client", time.Hour*24*(365+5),
 			x509.KeyUsageDigitalSignature|x509.KeyUsageKeyEncipherment,
 			[]x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth})
 	case "peer":
-		createCertificate(flag.Args()[2:], "ca/peer", "/CN=peer", 365+5,
+		createCertificate(flag.Args()[2:], "ca/peer", "/CN=peer", time.Hour*24*(365+5),
 			x509.KeyUsageDigitalSignature|x509.KeyUsageKeyEncipherment,
 			[]x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth})
 	case "signature":
-		createCertificate(flag.Args()[2:], "ca/sign", "/CN=sign", 365+5,
+		createCertificate(flag.Args()[2:], "ca/sign", "/CN=sign", time.Hour*24*(365+5),
 			x509.KeyUsageDigitalSignature, nil)
 	case "export":
 		exportCertificate(flag.Args()[2:])
@@ -64,4 +72,63 @@ func kubesec() {
 	default:
 		infoLog.Println("Usage: certshop ca | ica | server | client | signature | export")
 	}
+}
+
+func createCertificate(args []string, path string, defaultDN string, defaultValidity time.Duration, usage x509.KeyUsage, extUsage []x509.ExtKeyUsage) {
+
+}
+
+func createCA(args []string, path string, defaultDN string, defaultValidity time.Duration) {
+	fs := flag.NewFlagSet("command", flag.PanicOnError)
+	dn := fs.String("dn", defaultDN, "certificate subject")
+	maxPathLength := fs.Int("maxPathLength", 0, "max path length")
+	validity := fs.Duration("validity", defaultValidity, "certificate duration")
+
+	if err := fs.Parse(args); err != nil {
+		errorLog.Fatalf("Failed to parse command line arguments: %s", err)
+	}
+	if len(fs.Args()) > 1 {
+		errorLog.Fatalf("Invalid path %s", strings.Join(fs.Args(), ","))
+	} else if len(fs.Args()) == 1 {
+		path = filepath.Clean(fs.Arg(0))
+	}
+	infoLog.Printf("Creating Certificate Authority %s with Subject: %s\n", path, *dn)
+	if !overwrite {
+		if _, err := os.Stat(path); err == nil {
+			errorLog.Fatalf("Error: directory %s exists, use -overwrite flag to overwrite.", filepath.Join(root, path))
+		}
+	}
+	if err := os.MkdirAll(path, publicPerms); err != nil {
+		errorLog.Fatalf("Failed to create directory %s: %s", path, err)
+	}
+	notBefore := time.Now().UTC()
+
+	manifest := certManifest{
+		path: path,
+		key:  generateKey(),
+		cert: &x509.Certificate{
+			MaxPathLen:     *maxPathLength,
+			MaxPathLenZero: !(*maxPathLength > 0),
+			KeyUsage:       x509.KeyUsageCertSign | x509.KeyUsageCRLSign,
+			ExtKeyUsage:    []x509.ExtKeyUsage{x509.ExtKeyUsageOCSPSigning},
+			NotBefore:      notBefore,
+			NotAfter:       notBefore.Add(*validity),
+			SerialNumber:   generateSerial(),
+		},
+	}
+	if filepath.Dir(path) == "." {
+		manifest.signingCert = manifest.cert
+		manifest.signingKey = manifest.key
+		manifest.cert.Subject = parseDN(pkix.Name{}, *dn)
+	} else {
+		file := filepath.Dir(path)
+		file = filepath.Join(file, filepath.Base(file))
+		manifest.signingCert = readCert(file + ".pem")
+		manifest.signingKey = readKey(file + "-key.pem")
+		manifest.cert.Subject = parseDN(manifest.signingCert.Subject, *dn)
+	}
+
+	// manifest.parseSAN(sans)
+	manifest.sign()
+	manifest.save()
 }
